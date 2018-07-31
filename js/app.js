2d79038b255af395f07c44c51a78aa0a8b682e8e
Talytta Carvalho <talytta110@hotmail.com>
2018-07-31 13:45:44 -0300

    versão web

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// La funcionalidad de tu proyecto
+$(document).on("scroll", function() {
+
+	if($(document).scrollTop()>100) {
+		$("#menu").removeClass("menu-large").addClass("menu-small");
+	} else {
+		$("#menu").removeClass("menu-small").addClass("menu-large");
+	}
+	
+});
